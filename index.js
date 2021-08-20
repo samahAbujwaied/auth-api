34d2408f694cd89d9824f3d81126f8c5fc4d1d2a
‘samahAbujwaied’ <samah.abujweed@yahoo.com>
2021-08-20 04:17:54 +0300

    last edit

--- index.js
+++ index.js
@@ -3,9 +3,7 @@
 require("dotenv").config();
 const app = require("./auth-api/server");
 const { db } = require("./auth-api/models/index");
-app.get('/',(req,res)=>{
-  res.json('Is connected ')
-})
+
 db.sync().then(() => {
   app.start(process.env.PORT || 3002);
 });
